0f8acb11c42c58d9964aea0a849a2ce7523b3601
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-05-29 13:05:22 -0400

    aaa

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,10 +1,10 @@
-export function capitalizar(str) {
-	return str.replace(/\s+/g, " ").replace(/\w\S*/g, function (txt) {
+export function capitalizar(str: string) {
+	return str.replace(/\s+/g, " ").replace(/\w\S*/g, function (txt: string) {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
 	});
 }
 
-export function slug(str) {
+export function slug(str: string) {
 	return str
 		.toLowerCase()
 		.replace(/-/g, " ")
